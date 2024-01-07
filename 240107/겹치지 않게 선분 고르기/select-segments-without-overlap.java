2e45f70cb0befe2ae451832e6c84fef18bd32a55
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-01-07 17:19:38 +0900

    Title: 겹치지 않게 선분 고르기, Time: 112ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240107/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 240107/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -41,6 +41,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
+        if (n == 1) {
+            System.out.println(1);
+            
+            return;
+        }
         StringTokenizer st;
         pairs = new Pair[n];
 
