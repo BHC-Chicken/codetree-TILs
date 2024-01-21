208b7431ca4921ccb58c692cd1c965fec9fbf01d
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-01-21 19:53:10 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 103ms, Memory: 13MB, Status: Passed - CodeTree

--- 240121/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
+++ 240121/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
@@ -1,20 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(br.readLine());
-        int[] dp = new int[num + 1];
+        int N = Integer.parseInt(br.readLine());
+        boolean[] visited = new boolean[N+4];
+        int[] count = new int[N+4];
+        Queue<Integer> queue = new LinkedList<>();
 
-        for (int i = 2; i <= num; i++) {
-            dp[i] = dp[i - 1] + 1;
-            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        visited[N] = true;
+        queue.add(N);
+
+        while(!queue.isEmpty()) {
+            int num = queue.poll();
+
+            if(num == 1) {
+                System.out.println(count[1]);
+                return;
+            }
+
+            if(num % 3 == 0 && !visited[num/3]) {
+                visited[num/3] = true;
+                queue.add(num/3);
+                count[num/3] = count[num] + 1;
+            }
+
+            if(num % 2 == 0 && !visited[num/2]) {
+                visited[num/2] = true;
+                queue.add(num/2);
+                count[num/2] = count[num] + 1;
+            }
+
+            if(num - 1 > 0 && !visited[num-1]) {
+                visited[num-1] = true;
+                queue.add(num-1);
+                count[num-1] = count[num] + 1;
+            }
+
+            if(num + 1 < N+3 && !visited[num+1]) {
+                visited[num+1] = true;
+                queue.add(num+1);
+                count[num+1] = count[num] + 1;
+            }
         }
-
-        System.out.println(dp[num]);
     }
 }
