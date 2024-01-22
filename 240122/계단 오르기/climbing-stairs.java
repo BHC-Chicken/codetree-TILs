e1b97aa4e8fca281bed86a8ed444f1b9acae5769
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-01-22 15:38:20 +0900

    Title: 계단 오르기, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240122/계단 오르기/climbing-stairs.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[1001];
+
+        arr[0] = 0;
+        arr[1] = 0;
+        arr[2] = 1;
+        arr[3] = 1;
+        arr[4] = 1;
+
+        for (int i = 5; i <= num; i++) {
+            arr[i] = arr[i - 2] + arr[i - 3];
+        }
+
+        System.out.println(arr[num]);
+    }
+}
