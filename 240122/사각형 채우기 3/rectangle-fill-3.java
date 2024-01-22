16100b28193cc74258cc56bd45543afc4b8b196b
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-01-22 16:21:21 +0900

    Title: 사각형 채우기 3, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240122/사각형 채우기 3/rectangle-fill-3.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[1001];
+
+        arr[0] = 0;
+        arr[1] = 2;
+        arr[2] = 7;
+        arr[3] = 22;
+
+        for (int i = 4; i <= num; i++) {
+            arr[i] = (arr[i - 1] * 3 + 1) % 1000000007;
+        }
+
+        System.out.println(arr[num]);
+    }
+}
