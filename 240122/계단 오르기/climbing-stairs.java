f8dbc1bb1a89cb67ceb3dca039af034324ea1a01
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-01-22 15:40:55 +0900

    Title: 계단 오르기, Time: 100ms, Memory: 8MB, Status: Passed - CodeTree

--- 240122/계단 오르기/climbing-stairs.java
+++ 240122/계단 오르기/climbing-stairs.java
@@ -16,7 +16,7 @@ public class Main {
         arr[4] = 1;
 
         for (int i = 5; i <= num; i++) {
-            arr[i] = ((arr[i - 2] % 10007) + (arr[i - 3] % 10007)) % 10007;
+            arr[i] = (arr[i - 2] + arr[i - 3]) % 10007;
         }
 
         System.out.println(arr[num]);
