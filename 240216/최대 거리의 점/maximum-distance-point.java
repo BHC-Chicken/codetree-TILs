dda82f51709f48556fa49bf1da5f75883c09d717
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-02-16 02:43:36 +0900

    Title: 최대 거리의 점, Time: 485ms, Memory: 22MB, Status: Passed - CodeTree

---
+++ 240216/최대 거리의 점/maximum-distance-point.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int m;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        int left = 1;
+        int right = arr[n - 1] - arr[0];
+        int result = 0;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int prev = arr[0];
+            int count = 1;
+
+            for (int i = 1; i < n; i++) {
+                if (arr[i] - prev >= mid) {
+                    count++;
+                    prev = arr[i];
+                }
+            }
+
+            if (count >= m) {
+                left = mid + 1;
+                result = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
