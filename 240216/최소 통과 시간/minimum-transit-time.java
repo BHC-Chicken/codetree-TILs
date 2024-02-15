a378a9ac22f02dfa7d7ab28433e57aaf9594469e
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-02-16 03:10:31 +0900

    Title: 최소 통과 시간, Time: 213ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240216/최소 통과 시간/minimum-transit-time.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int m;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        int left = 1;
+        int right = n * arr[m - 1];
+        int result = 0;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int sum = 0;
+
+            for (int i = 0; i < m; i++) {
+                sum += mid / arr[i];
+            }
+
+            if (sum >= n) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
