af44e5739118072b70cd45c2c092d7848b9d6ab3
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-02-15 21:03:41 +0900

    Title: 정수 분배하기, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240215/정수 분배하기/distributing-integers.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int m;
+    static int[] arr;
+
+    static boolean isPossible(int maxDiv) {
+        int part = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < maxDiv) {
+                return false;
+            }
+            int num = arr[i];
+
+            while (num >= maxDiv) {
+                num -= maxDiv;
+                part++;
+            }
+        }
+
+        return part >= m;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        int minValue = Integer.MAX_VALUE;
+        int result = Integer.MIN_VALUE;
+
+        arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+
+            minValue = Math.min(minValue, arr[i]);
+        }
+
+        int left = 1;
+        int right = minValue;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (isPossible(mid)) {
+                left = mid + 1;
+                result = Math.max(result, mid);
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
