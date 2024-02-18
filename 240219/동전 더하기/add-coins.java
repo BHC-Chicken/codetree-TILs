8809501ab50974d8f0a9eb3c62e9e129c2b376c2
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-02-19 02:36:11 +0900

    Title: 동전 더하기, Time: 110ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240219/동전 더하기/add-coins.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int index = n - 1;
+        int count = 0;
+
+        while (k > 0) {
+            int temp = k / arr[index];
+            count +=temp;
+            k -= arr[index--] * temp;
+        }
+
+        System.out.println(count);
+    }
+}
