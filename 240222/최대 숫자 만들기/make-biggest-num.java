c12c35b9fc911427b95ad13d1342d00f59121b63
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-02-22 22:36:42 +0900

    Title: 최대 숫자 만들기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240222/최대 숫자 만들기/make-biggest-num.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return Integer.parseInt(b + a) - Integer.parseInt(a + b);
+            }
+        });
+
+        for (String s : arr) {
+            sb.append(s);
+        }
+
+        System.out.println(sb);
+    }
+}
