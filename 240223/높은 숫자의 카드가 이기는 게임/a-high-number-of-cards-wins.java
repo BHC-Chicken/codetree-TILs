1568a2ca4a891c09085de73d2d66ae7592e737f9
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-02-23 00:49:29 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240223/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.java
+++ 240223/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.java
@@ -7,23 +7,37 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
         int score = 0;
 
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            arr1[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.sort(arr);
+        Arrays.sort(arr1);
+        int index1 = 0;
+        int index2 = 0;
 
-        for (int i = 0; i < n - 1; i++) {
-            if (arr[i + 1] > arr[i] + 1) {
-                score++;
+        for (int i = 1; i <= n * 2; i++) {
+            if (arr1[index2] != i) {
+                arr2[index1++] = i;
+
+            } else {
+                index2++;
+
+                if (index2 == n) {
+                    index2 = (n - 1);
+                }
             }
         }
 
-        if (arr[n - 1] < 2 * n) {
-            score++;
+        Arrays.sort(arr2);
+
+        for (int i = 0; i < n; i++) {
+            if (arr1[i] < arr2[i]) {
+                score++;
+            }
         }
 
         System.out.println(score);
