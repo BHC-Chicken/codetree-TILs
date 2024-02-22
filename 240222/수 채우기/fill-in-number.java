863c5b2aabd13814b21aca3fcb21e1e40918e953
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-02-22 23:02:41 +0900

    Title: 수 채우기, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240222/수 채우기/fill-in-number.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        int result = 0;
+
+        if (num % 2 == 0) {
+            result += num / 5;
+            num = num - (num / 5 * 5);
+            result += num / 2;
+            num = num - (num / 2 * 2);
+        } else {
+            result += num / 5;
+            result--;
+            num = num - (num / 5 * 5);
+            num += 5;
+            result += num / 2;
+            num = num - (num / 2 * 2);
+        }
+
+        if (num == 0) {
+            System.out.println(result);
+        } else {
+            System.out.println(-1);
+        }
+    }
+}
