4de0006c08cb20e1d470cd7de8282d567c8f3c8c
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-03-03 01:19:31 +0900

    Title: 좌우 반전시키기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240303/좌우 반전시키기/flip-left-and-right.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        char[] arr = br.readLine().replaceAll(" ", "").toCharArray();
+        boolean reverse = false;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == '0') {
+                count++;
+                i++;
+
+                arr[i] = arr[i] == '0' ? '1' : '0';
+            }
+        }
+
+        if (arr[n - 1] == '0') {
+            System.out.println(-1);
+        } else {
+            System.out.println(count);
+        }
+    }
+}
