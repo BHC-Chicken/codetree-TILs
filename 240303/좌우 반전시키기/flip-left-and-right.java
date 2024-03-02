546a54c30a2a16cc6b53ef44d8585d3830845a5e
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-03-03 01:31:06 +0900

    Title: 좌우 반전시키기, Time: 97ms, Memory: 9MB, Status: Passed - CodeTree

--- 240303/좌우 반전시키기/flip-left-and-right.java
+++ 240303/좌우 반전시키기/flip-left-and-right.java
@@ -8,19 +8,16 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
 
         char[] arr = br.readLine().replaceAll(" ", "").toCharArray();
-        boolean reverse = false;
         int count = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == '0') {
+        for (int i = 1; i < n; i++) {
+            if (arr[i - 1] == '0') {
                 count++;
-                i++;
-
-                if (i >= n) {
-                    break;
-                }
 
                 arr[i] = arr[i] == '0' ? '1' : '0';
+                if (i + 1 < n) {
+                    arr[i + 1] = arr[i + 1] == '0' ? '1' : '0';
+                }
             }
         }
 
