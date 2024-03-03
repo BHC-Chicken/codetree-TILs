03d03a6e12429a11e9208e13519096c825b5c098
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-03-04 00:56:59 +0900

    Title: 숫자 등장 횟수, Time: 449ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240304/숫자 등장 횟수/number-frequency.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<Integer, Integer> map = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        while (st.hasMoreTokens()) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int temp = Integer.parseInt(st.nextToken());
+
+            if (map.containsKey(temp)) {
+                sb.append(map.get(temp)).append(" ");
+            } else {
+                sb.append(0).append(" ");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
