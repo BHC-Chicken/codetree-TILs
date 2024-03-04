a8e6712766aea280c0ca004a3d6bb46de9708e42
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-03-04 18:26:42 +0900

    Title: 두 수의 합, Time: 500ms, Memory: 27MB, Status: Passed - CodeTree

--- 240304/두 수의 합/sum-of-two-num.java
+++ 240304/두 수의 합/sum-of-two-num.java
@@ -8,28 +8,33 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Long, Integer> map = new HashMap<>();
 
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
+        long[] arr = new long[n];
         int result = 0;
 
         st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(st.nextToken());
+            long num = Long.parseLong(st.nextToken());
 
             arr[i] = num;
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
         for (int i = 0; i < n; i++) {
-            int temp = Math.abs(k - arr[i]);
+            long temp = k - arr[i];
 
-            if (map.containsKey(temp)) {
+            if (map.containsKey(temp) && map.get(arr[i]) > 0) {
                 result += map.get(temp);
 
-                map.remove(arr[i]);
+                if (arr[i] == temp) {
+                    result--;
+                }
+
+                map.put(arr[i], map.get(arr[i]) - 1);
             }
         }
 
