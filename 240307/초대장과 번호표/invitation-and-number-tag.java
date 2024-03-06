a61dc8a7d210e4785f2e3e53c3b11d14995a7b9d
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-03-07 02:37:02 +0900

    Title: 초대장과 번호표, Time: 1143ms, Memory: 53MB, Status: Passed - CodeTree

--- 240307/초대장과 번호표/invitation-and-number-tag.java
+++ 240307/초대장과 번호표/invitation-and-number-tag.java
@@ -1,7 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,28 +14,57 @@ public class Main {
 
         int n = Integer.parseInt(st.nextToken());
         int g = Integer.parseInt(st.nextToken());
-        HashSet<Integer> hashSet = new HashSet<>();
-        hashSet.add(1);
+        int answer = 0;
+
+        ArrayList<Integer>[] people = new ArrayList[n];
+        HashSet<Integer>[] group = new HashSet[g];
+
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] invite = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            people[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < g; i++) {
+            group[i] = new HashSet<>();
+        }
 
         for (int i = 0; i < g; i++) {
             st = new StringTokenizer(br.readLine());
 
             int num = Integer.parseInt(st.nextToken());
-            HashSet<Integer> temp = new HashSet<>();
 
             for (int j = 0; j < num; j++) {
-                temp.add(Integer.parseInt(st.nextToken()));
-            }
-
-            for (int t : hashSet) {
-                temp.remove(t);
-            }
-
-            if (temp.size() == 1) {
-                hashSet.addAll(temp);
+                int p = Integer.parseInt(st.nextToken()) - 1;
+                group[i].add(p);
+                people[p].add(i);
             }
         }
 
-        System.out.println(hashSet.size());
+        queue.add(0);
+        invite[0] = true;
+
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            answer++;
+
+            for (int i = 0; i < people[curr].size(); i++) {
+                int groupNumber = people[curr].get(i);
+
+                group[groupNumber].remove(curr);
+
+                if (group[groupNumber].size() == 1) {
+                    int person = new ArrayList<>(group[groupNumber]).get(0);
+
+                    if (!invite[person]) {
+                        invite[person] = true;
+                        queue.add(person);
+                    }
+                }
+            }
+        }
+
+        System.out.println(answer);
     }
 }
