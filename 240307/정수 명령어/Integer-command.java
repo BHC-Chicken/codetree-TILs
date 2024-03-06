7f541270277ac14da8fd287435335e1908b86211
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-03-07 04:47:05 +0900

    Title: 정수 명령어, Time: 570ms, Memory: 28MB, Status: Passed - CodeTree

--- 240307/정수 명령어/Integer-command.java
+++ 240307/정수 명령어/Integer-command.java
@@ -7,12 +7,12 @@ import java.util.TreeSet;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        TreeSet<Integer> treeSet = new TreeSet<>();
         int t = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
         for (int i = 0; i < t; i++) {
+            TreeSet<Integer> treeSet = new TreeSet<>();
             int loop = Integer.parseInt(br.readLine());
 
             for (int j = 0; j < loop; j++) {
