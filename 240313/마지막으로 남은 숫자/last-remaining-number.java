af2fa07f83ffc321b6bd1cde58fa1d13e7aaedb5
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-03-13 03:46:14 +0900

    Title: 마지막으로 남은 숫자, Time: 531ms, Memory: 22MB, Status: Passed - CodeTree

--- 240313/마지막으로 남은 숫자/last-remaining-number.java
+++ 240313/마지막으로 남은 숫자/last-remaining-number.java
@@ -1,25 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
 
         int n = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         while (st.hasMoreTokens()) {
-            queue.add(-Integer.parseInt(st.nextToken()));
+            queue.add(Integer.parseInt(st.nextToken()));
         }
 
         while (queue.size() > 1) {
-            int num1 = -queue.poll();
-            int num2 = -queue.poll();
+            int num1 = queue.poll();
+            int num2 = queue.poll();
 
             int sum = num1 - num2;
 
@@ -27,13 +28,13 @@ public class Main {
                 continue;
             }
 
-            queue.add(-sum);
+            queue.add(sum);
         }
 
         if (queue.isEmpty()) {
             System.out.println(-1);
         } else {
-            System.out.println(-queue.poll());
+            System.out.println(queue.poll());
         }
     }
 }
