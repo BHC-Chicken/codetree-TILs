9ac63e6bf8766a461bac192330d73bc4c4a2f579
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-03-13 03:10:46 +0900

    Title: 가장 가까운 점, Time: 473ms, Memory: 23MB, Status: Passed - CodeTree

--- 240313/가장 가까운 점/nearest-point.java
+++ 240313/가장 가까운 점/nearest-point.java
@@ -7,19 +7,23 @@ import java.util.StringTokenizer;
 class Pair implements Comparable<Pair> {
     int x;
     int y;
+    int sum;
 
-    public Pair(int x, int y) {
+    public Pair(int x, int y, int sum) {
         this.x = x;
         this.y = y;
+        this.sum = sum;
     }
 
     @Override
     public int compareTo(Pair o) {
-        if (this.x == o.x) {
+        if (this.sum == o.sum) {
+            return this.x - this.y;
+        } else if (this.x == this.y) {
             return this.y - o.y;
         }
 
-        return this.x - o.x;
+        return this.sum - o.sum;
     }
 }
 
@@ -36,13 +40,19 @@ public class Main {
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
 
-            queue.add(new Pair(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            queue.add(new Pair(x, y, Math.abs(x) + Math.abs(y)));
         }
 
         for (int i = 0; i < m; i++) {
             Pair pair = queue.poll();
 
-            queue.add(new Pair(pair.x + 2, pair.y + 2));
+            int x = pair.x + 2;
+            int y = pair.y + 2;
+
+            queue.add(new Pair(x, y, x + y));
         }
 
         Pair pair = queue.poll();
