61bb2c0f71dbaa9a0949ea7437c8d2f27092cf8f
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-03-13 03:37:33 +0900

    Title: 마지막으로 남은 숫자, Time: 102ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240313/마지막으로 남은 숫자/last-remaining-number.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        while (st.hasMoreTokens()) {
+            queue.add(-Integer.parseInt(st.nextToken()));
+        }
+
+        while (queue.size() > 1) {
+            int num1 = -queue.poll();
+            int num2 = -queue.poll();
+            int sum = Math.abs(num1 - num2);
+
+            if (sum == 0) {
+                continue;
+            }
+
+            queue.add(sum);
+
+            if (queue.isEmpty()) {
+                System.out.println(-1);
+
+                return;
+            }
+        }
+
+        System.out.println(queue.poll());
+    }
+}
