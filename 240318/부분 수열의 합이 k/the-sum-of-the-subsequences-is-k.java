cb9261e7a912dcca0a24e0dab3a5ec9802c80382
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-03-18 23:02:26 +0900

    Title: 부분 수열의 합이 k, Time: 116ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240318/부분 수열의 합이 k/the-sum-of-the-subsequences-is-k.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n + 1];
+        arr[0] = 0;
+        int count = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j <= n; j++) {
+                int sum = arr[j] - arr[j - i];
+
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
