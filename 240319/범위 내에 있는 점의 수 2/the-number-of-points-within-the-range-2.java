1c1a10d119e1ad9125cd60897c8afd09490ab1c0
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-03-19 17:09:32 +0900

    Title: 범위 내에 있는 점의 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240319/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] arr;
+    static int[] sum;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+        int max = Integer.MIN_VALUE;
+
+        sum = new int[100001];
+        arr = new int[100001];
+
+        sum[0] = 0;
+        arr[0] = 0;
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            max = Math.max(max, num);
+            arr[num] = 1;
+        }
+
+        for (int i = 1; i <= max; i++) {
+            if (arr[i] == 1) {
+                sum[i] = sum[i - 1] + 1;
+            } else {
+                sum[i] = sum[i - 1];
+            }
+        }
+
+        for (int i = 0; i < q; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            if (s > max) {
+                sb.append(0).append("\n");
+            } else {
+                sb.append(sum[e] - sum[s - 1]).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
