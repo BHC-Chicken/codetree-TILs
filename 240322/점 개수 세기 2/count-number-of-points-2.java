d82a028c4026addf14a8161560ba6ab3443d88ee
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-03-22 19:24:30 +0900

    Title: 점 개수 세기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240322/점 개수 세기 2/count-number-of-points-2.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+class Pair implements Comparable<Pair> {
+    int x;
+    int y;
+
+    public Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Pair o) {
+        return this.x - o.x;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        TreeSet<Integer> treeSetX = new TreeSet<>();
+        TreeSet<Integer> treeSetY = new TreeSet<>();
+        TreeSet<Pair> pairTreeSet = new TreeSet<>();
+
+        HashMap<Integer, Integer> mapX = new HashMap<>();
+        HashMap<Integer, Integer> mapY = new HashMap<>();
+
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+
+        Pair[] pairs1 = new Pair[n + 1];
+        Pair[] pairs2 = new Pair[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            treeSetX.add(x);
+            treeSetY.add(y);
+            pairTreeSet.add(new Pair(x, y));
+        }
+
+        for (int i = 0; i < q; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            pairs1[i] = new Pair(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            pairs2[i] = new Pair(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+
+        int index = 1;
+        for (Integer num : treeSetX) {
+            mapX.put(num, index);
+            index++;
+        }
+
+        index = 1;
+        for (Integer num : treeSetY) {
+            mapY.put(num, index);
+            index++;
+        }
+
+        for (int i = 0; i < q; i++) {
+            int count = 0;
+            int x1 = treeSetX.ceiling(pairs1[i].x);
+            int y1 = treeSetY.ceiling(pairs1[i].y);
+
+            int x2 = treeSetX.floor(pairs2[i].x);
+            int y2 = treeSetY.floor(pairs2[i].y);
+
+            for (Pair pair : pairTreeSet) {
+                if (pair.x >= x1 && pair.x <= x2 && pair.y >= y1 && pair.y <= y2) {
+                    count++;
+                }
+            }
+
+            sb.append(count).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
