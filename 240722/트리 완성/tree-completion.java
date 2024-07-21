0c429894fa8e65c3ccf1a47b515f8301f7961e07
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-07-22 01:55:47 +0900

    Title: 트리 완성, Time: 530ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240722/트리 완성/tree-completion.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int m;
+    static int[] arr;
+    static boolean circle;
+
+    static int find(int a) {
+        if (a == arr[a]) {
+            return a;
+        }
+
+        return arr[a] = find(arr[a]);
+    }
+
+    static void union(int a, int b) {
+        int findA = find(a);
+        int findB = find(b);
+
+        if (findA != findB) {
+            arr[findA] = findB;
+        } else {
+            circle = true;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = i;
+        }
+
+        int cutCount = 0;
+
+        for (int i = 1; i <= m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (find(a) == find(b)) {
+                cutCount++;
+                continue;
+            }
+
+            union(a, b);
+        }
+
+        int plusCount = 0;
+        int prev = find(arr[1]);
+
+        for (int i = 2; i <= n; i++) {
+            if (prev != find(arr[i])) {
+                prev = find(arr[i]);
+                union(i - 1, i);
+                plusCount++;
+            }
+        }
+
+        System.out.println(cutCount + plusCount);
+    }
+}
