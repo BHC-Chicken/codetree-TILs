a5866431d42d77759b2801ac35ba8e6a45a0d1e4
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-07-22 01:23:27 +0900

    Title: 정점 연결하기, Time: 426ms, Memory: 22MB, Status: Passed - CodeTree

---
+++ 240722/정점 연결하기/connect-vertices.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int[] arr;
+
+    static int find(int a) {
+        if (a == arr[a]) {
+            return a;
+        }
+
+        return arr[a] = find(arr[a]);
+    }
+
+    static void union(int a, int b) {
+        int findA = find(a);
+        int findB = find(b);
+
+        if (findA != findB) {
+            arr[findA] = findB;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = 0; i < n -2 ; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            union(a,b);
+        }
+
+        boolean findCut = false;
+
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 1; j <= n ; j++) {
+                if (find(i) != find(j)) {
+                    sb.append(i).append(" ").append(j);
+                    findCut = true;
+
+                    break;
+                }
+            }
+            if (findCut) {
+                break;
+            }
+        }
+
+
+        System.out.println(sb);
+    }
+}
