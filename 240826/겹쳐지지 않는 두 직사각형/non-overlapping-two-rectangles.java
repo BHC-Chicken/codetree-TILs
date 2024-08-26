4111cf469179ce8f595a4dc99cdd3c320208d5f4
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-08-26 18:28:36 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 193ms, Memory: 10MB, Status: Passed - CodeTree

--- 240826/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.java
+++ 240826/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.java
@@ -1,96 +1,103 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-    static int n;
-    static int m;
-    static int[][] map = new int[5][5];
-    static int[][] board = new int[5][5];
-
+    public static final int INT_MIN = Integer.MIN_VALUE;
+    public static final int MAX_NUM = 5;
+    
+    public static int n, m;
+    public static int[][] grid = new int[MAX_NUM][MAX_NUM];
+    public static int[][] board = new int[MAX_NUM][MAX_NUM];
+    
     public static void clearBoard() {
         for(int i = 0; i < n; i++)
             for(int j = 0; j < m; j++)
                 board[i][j] = 0;
     }
-
-    static void draw(int x1, int y1, int x2, int y2) {
+    
+    public static void draw(int x1, int y1, int x2, int y2) {
         for(int i = x1; i <= x2; i++)
             for(int j = y1; j <= y2; j++)
                 board[i][j]++;
     }
-
-    static boolean checkBoard() {
+    
+    public static boolean checkBoard() {
+        // 동일한 칸을 2개의 직사각형이 모두 포함한다면
+        // 겹치게 됩니다.
         for(int i = 0; i < n; i++)
             for(int j = 0; j < m; j++)
                 if(board[i][j] >= 2)
                     return true;
         return false;
     }
-
-    static boolean overlapped(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
+    
+    // (x1, y1), (x2, y2) 그리고
+    // (x3, y3), (x4, y4) 로 이루어져있는
+    // 두 직사각형이 겹치는지 확인하는 함수
+    public static boolean overlapped(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
         clearBoard();
-
         draw(x1, y1, x2, y2);
         draw(x3, y3, x4, y4);
-
         return checkBoard();
     }
-
-    static int rectSum(int x1, int y1, int x2, int y2) {
+    
+    public static int rectSum(int x1, int y1, int x2, int y2) {
         int sumOfNums = 0;
         for(int i = x1; i <= x2; i++)
             for(int j = y1; j <= y2; j++)
-                sumOfNums += map[i][j];
-
+                sumOfNums += grid[i][j];
+        
         return sumOfNums;
     }
-
-    static int findMaxSum(int x1, int y1, int x2, int y2) {
-        int maxSum = Integer.MIN_VALUE;
-
+    
+    // 첫 번째 직사각형이 (x1, y1), (x2, y2)를 양쪽 꼭지점으로 할 때
+    // 두 번째 직사각형을 겹치지 않게 잘 잡아
+    // 최대 합을 반환하는 함수
+    public static int findMaxSum(int x1, int y1, int x2, int y2) {
+        int maxSum = INT_MIN;
+        
+        // (i, j), (k, l)을 양쪽 꼭지점으로 하는
+        // 두 번째 직사각형을 정하여
+        // 겹치지 않았을 때 중
+        // 최댓값을 찾아 반환합니다.
         for(int i = 0; i < n; i++)
             for(int j = 0; j < m; j++)
                 for(int k = i; k < n; k++)
                     for(int l = j; l < m; l++) {
                         if(!overlapped(x1, y1, x2, y2, i, j, k, l))
-                            maxSum = Math.max(maxSum, rectSum(x1, y1, x2, y2) + rectSum(i, j, k, l));
+                            maxSum = Math.max(maxSum, 
+                                          rectSum(x1, y1, x2, y2) +
+                                          rectSum(i, j, k, l));
                     }
-
+        
+        return maxSum;
+    }
+    
+    // 두 직사각형을 잘 잡았을 때의 최대 합을 반환하는 함수
+    public static int findMaxSum() {
+        int maxSum = INT_MIN;
+        
+        // (i, j), (k, l)을 양쪽 꼭지점으로 하는
+        // 첫 번째 직사각형을 정하여
+        // 그 중 최댓값을 찾아 반환합니다.
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                for(int k = i; k < n; k++)
+                    for(int l = j; l < m; l++)
+                        maxSum = Math.max(maxSum,
+                                      findMaxSum(i, j, k, l));
         return maxSum;
     }
 
-    static int maxSum() {
-        int max = Integer.MIN_VALUE;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                for (int k = i; k < n; k++) {
-                    for (int l = j; l < m; l++) {
-                        max = Math.max(max, findMaxSum(i,j,k,l));
-                    }
-                }
-            }
-        }
-
-        return max;
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        System.out.println(maxSum());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                grid[i][j] = sc.nextInt();
+        
+        int ans = findMaxSum();
+        System.out.print(ans);
     }
 }
