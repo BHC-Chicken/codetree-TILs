99ef7e1e0045cb6e86d37a18eaae8ba4bb73711f
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-10-19 01:36:54 +0900

    Title: 선분 위의 점, Time: 632ms, Memory: 27MB, Status: Passed - CodeTree

--- 241019/선분 위의 점/point-on-the-line-segment.java
+++ 241019/선분 위의 점/point-on-the-line-segment.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -62,14 +63,13 @@ public class Main {
             map[i] = Integer.parseInt(st.nextToken());
         }
 
+        Arrays.sort(map);
+
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
 
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-
-            int lower = lower(start);
-            int upper = upper(end);
+            int lower = lower(Integer.parseInt(st.nextToken()));
+            int upper = upper(Integer.parseInt(st.nextToken()));
 
             sb.append(upper - lower).append("\n");
         }
